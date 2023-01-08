de7397357404bfc868116f343faa0c44f00c24ed
Jabjabrik <Yusufjabriko>
2023-01-08 16:19:38 +0700

    initial 1.2

--- app/app.js
+++ app/app.js
@@ -1,26 +1,6 @@
-/**
- *
- * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
- * @param {Object} event - API Gateway Lambda Proxy Input Format
- *
- * Context doc: https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-context.html 
- * @param {Object} context
- *
- * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
- * @returns {Object} object - API Gateway Lambda Proxy Output Format
- * 
- */
+import express from 'express'
+import serverless from 'serverless-http'
 
-export const handler = async (event, context) => {
-    try {
-        return {
-            'statusCode': 200,
-            'body': JSON.stringify({
-                message: 'hello world 234',
-            })
-        }
-    } catch (err) {
-        console.log(err);
-        return err;
-    }
-};
+const app = express()
+app.get('/', (req, res) => res.json({ message: 'hello world new' }))
+export const handler = serverless(app)
