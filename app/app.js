09e72dde3f516a9c173b5b05c896985719177ed6
Jabjabrik <Yusufjabriko>
2023-01-08 16:50:04 +0700

    initial 1.4

--- app/app.js
+++ app/app.js
@@ -2,5 +2,5 @@ import express from 'express'
 import serverless from 'serverless-http'
 
 const app = express()
-app.get('/', (req, res) => res.json({ message: 'hello world new' }))
+app.get('/', (req, res) => res.json({ message: 'hello world siuu' }))
 export const handler = serverless(app)
